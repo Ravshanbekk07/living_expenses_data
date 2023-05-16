3434cd0423b2ed42da2e85f1090ca7d5160f235f
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-16 14:19:35 +0500

    cool

--- average.py
+++ average.py
@@ -10,7 +10,18 @@ def average_expenses(file_path: str) -> float:
     Returns:
         float: average expenses
     """
-    pass
+    f = open(file_path)
+    x = f.read()
+    jason_data = json.loads(x)
+    c = list(jason_data.values())
+    #print(len(c))
+    u = 0
+    for i in c:
+        u+=i
+    return u/len(c)
+
+
+
 
 # test
 file_path = "data.json"
